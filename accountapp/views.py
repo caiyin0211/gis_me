e80280f453ba304e92a64f922cc5a2258f54b187
caiyin0211 <caiyin0211@gmail.com>
2021-07-13 14:45:26 +0900

    7/13 commit1

--- accountapp/views.py
+++ accountapp/views.py
@@ -26,7 +26,7 @@ def hello_world(request):
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
 
-class AccountCreaeView(CreateView):
+class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
